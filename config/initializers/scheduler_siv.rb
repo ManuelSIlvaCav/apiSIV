c2bf53b74ce2672a1b9fd4868b47f9f7144c937b
ManuelSIlvaCav <msilvacav@gmail.com>
2018-09-21 16:51:01 -0400

    push api

---
+++ config/initializers/scheduler_siv.rb
@@ -0,0 +1,21 @@
+require 'rufus-scheduler'
+require 'rubygems'
+
+
+
+Rufus::Scheduler.new(max_worker_threads: 1).every '8s', overlap: false, first_in: '5s'do
+  p 'ITERACION SCHEDULER'
+  database = Mdb.open("../../../cotizaC.mdb")
+  #Rails.logger.info 'ITERACION SCHEDULER2'
+
+  #TABLAS CON info
+  # clientesn
+  # clientesSIV
+  # entregas
+  # historial, historialc, 
+  # importasc1,2,3
+  # pc, pp, sc
+  #scproduccion, scx
+
+
+end
