9727ada74144caf05b65f34d9c0b1d96ac254f21
ManuelSIlvaCav <msilvacav@gmail.com>
2018-09-21 21:14:12 -0400

    carga Sc

--- app/workers/hello_world.rb
+++ app/workers/hello_world.rb
@@ -6,8 +6,26 @@ class HelloWorld
   include Sidekiq::Worker
 
   def perform
+    params = Hash.new
     db = Mdb.open("../../cotizaC.mdb")
-    puts db[:sc][0]
-    #puts 'Hello-World'
+    #db = Mdb.open(ruta absoluta en servidor)
+    scs = db[:sc]
+    scs.each do |row|
+      if Sc.where(:siv_id => row[:numsiv]).empty?
+        params[:siv_id] = row[:numsiv]
+        params[:qad_id] = row[:codigoqad]#row[:numqad]
+        params[:rut] = row[:rutc]
+        params[:client_name] = row[:cliente]
+        params[:name] = row[:producto]
+        params[:sku] = row[:sku]
+        params[:umx] = row[:umx]
+        params[:siv_code] = row[:codigoqad]
+        params[:kind] = row[:tipoxx]
+        params[:sub_kind] = row[:subtipoxx]
+        params[:materials] = row[:materiales]
+        params[:structure] = row[:estructurap]
+        Sc.create(params)
+      end
+    end
   end
 end
